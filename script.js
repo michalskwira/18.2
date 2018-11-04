2bf02141f5f3a98705713b4ffa5060e2e5223f95
Michał Skwira <michalskwira@yahoo.com>
2018-11-04 09:50:57 +0100

    Task 18.2 done

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-
+'use scrict';
 var element =
   React.createElement('div', {},
     React.createElement('h1', {}, 'Lista filmów'),
-    React.createElement('ul', {},
-      React.createElement('li', {},
-        React.createElement('h2', {}, 'Harry Potter'),
-        React.createElement('p', {}, 'Film o czarodzieju')
-      ),
-      React.createElement('li', {},
-        React.createElement('h2', {}, 'Król Lew'),
-        React.createElement('p', {}, 'Film opowiadający historię króla sawanny')
-      )
-    )
   );
+var movies = [
+  {
+    id: 1,
+    title: 'Harry Potter',
+    desc: 'film o czarodzieju',
+    image: 'https://1.fwcdn.pl/po/05/71/30571/7529392.6.jpg'
+  },
+  {
+    id: 2,
+    title: 'Król Lew',
+    desc: 'Film o królu sawanny',
+    image: 'http://s3.viva.pl/newsy/krol-lew-plakat-filmu-427851-movie_cover.jpg'
+  },
+  {
+  	id: 3,
+  	title: 'Świnka Pepa',
+  	desc: 'bajka o śwince',
+  	image: 'https://eplakaty.pl/img/towary/2/2016_11/43997.jpg'
+  },
+
+];
+var moviesElements = movies.map(function(movie) {
+  return React.createElement('li', {key: movie.id},
+      React.createElement('h2', {}, movie.title),
+      React.createElement('p', {}, movie.desc),
+      React.createElement('img',{src: movie.image})
+    );
+});
+var element =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'Lista filmów'),
+    React.createElement('ul', {}, moviesElements),
+  );
+
+  ReactDOM.render(element, document.getElementById('app'));
+
+
+  
