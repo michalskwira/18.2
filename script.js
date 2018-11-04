bf375d7bf7d166e2c72130e12270e1b260dddb97
Michał Skwira <michalskwira@yahoo.com>
2018-11-04 09:04:54 +0100

    Change script.js

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-var element = React.createElement('div', {}, 'Hello world!');
-ReactDOM.render(element, document.getElementById('app'))
+
+var element =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'Lista filmów'),
+    React.createElement('ul', {},
+      React.createElement('li', {},
+        React.createElement('h2', {}, 'Harry Potter'),
+        React.createElement('p', {}, 'Film o czarodzieju')
+      ),
+      React.createElement('li', {},
+        React.createElement('h2', {}, 'Król Lew'),
+        React.createElement('p', {}, 'Film opowiadający historię króla sawanny')
+      )
+    )
+  );
